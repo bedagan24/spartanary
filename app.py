6b46e8790f3ee631b142818490cd13a55347bd69
bedagan24 <bedagan24@students.unnes.ac.id>
2023-11-14 12:44:30 +0700

    First Commit

--- app.py
+++ app.py
@@ -3,13 +3,23 @@ from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
-password ='test'
-cxn_str=f'mongodb://lxfarhan:{password}@ac-xafbpj1-shard-00-00.q0qvbkc.mongodb.net:27017,ac-xafbpj1-shard-00-01.q0qvbkc.mongodb.net:27017,ac-xafbpj1-shard-00-02.q0qvbkc.mongodb.net:27017/?ssl=true&replicaSet=atlas-erdgvk-shard-0&authSource=admin&retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
-db = client.dbsparta_plus_week2
+
 
 
 @app.route('/')
